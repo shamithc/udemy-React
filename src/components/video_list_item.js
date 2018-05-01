771a35bdb0b894eb716d7560bf36b90f07cac7c3
Shamith c <shamithc@gmail.com>
2018-05-02 00:07:03 +0530

    Add ListItem key

---
+++ src/components/video_list_item.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const VideListItem = (props) => {
+    return (
+        <li>video</li>
+    )
+}
+
+export default VideListItem;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyDOWC4EQ72yLXG-5Rx1QmvMyhVkuEYkg_4';
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>    
-    ); 
+class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {videos: [] };
+
+        YTSearch(
+            {key: API_KEY, term: 'abc' }, videos => {
+                this.setState({  videos });
+            }
+        );
+    }
+
+    render(){
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
+            </div>    
+        ); 
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
