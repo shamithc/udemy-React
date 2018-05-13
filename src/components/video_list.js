16544b66ca211ad65f11fa27006bac36a9f95951
Shamith c <shamithc@gmail.com>
2018-05-13 13:41:04 +0530

    Show video-details on click video-list + styles

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import VideoListItem from './video_list_item';
 
-const VideoList = (props) => {
-  const VideoListItems = props.videos.map((video) => {
-    return <VideoListItem key={video.etag} video={video} />
+const VideoList = ({videos, onVideoSelect}) => {
+  const VideoListItems = videos.map((video) => {
+    return <VideoListItem onVideoSelect={onVideoSelect} key={video.etag} video={video} />
   })
   return (
     <ul className="col-md-4 list-group">
--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const VideListItem = ({video}) => {
+const VideListItem = ({video, onVideoSelect}) => {
   const imageUrl = video.snippet.thumbnails.default.url;
   return (
-    <li className="list-group-item">
+    <li onClick={() => onVideoSelect(video)} className="list-group-item">
       <div className="video-list media">
         <div className="media-left">
           <img className="media-object" src={ imageUrl } />
